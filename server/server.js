5432e5c38e1f1669edc691ac00f4c52457233cfc
Tucker Navin <tuckernavin@gmail.com>
2026-04-06 22:45:38 -0600

    fix public directory path

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const publicDir = path.join(__dirname, '..', 'public');
+const publicDir = path.join(__dirname, 'public');
 const PORT = process.env.PORT || 3000;
 const MLB_BASE = 'https://statsapi.mlb.com/api/v1';
 
